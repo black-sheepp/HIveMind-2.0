ba1610888634a68fbd1f957fb68925208e201b6a
Shivam <shivamguptanitw@gmail.com>
2023-04-30 12:44:32 +0530

    Commit 4: Express layout, Static folder Assets for CSS are setup

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const App = express();
 const Port = 8080;
+const expressLayouts = require('express-ejs-layouts');
+
+App.set('view engine', 'ejs');
+
+App.use(express.static('./Assets'))
+App.use(expressLayouts);
+
+
+
+
+
 
 App.use('/',require('./Routes'))
 
