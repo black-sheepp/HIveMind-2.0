73f99292f518f807c78a8b03ec8d797f0468018e
Shivam <shivamguptanitw@gmail.com>
2023-05-05 23:38:36 +0530

    Commit 12: Display post and related users on dashboard

--- Controller/user.js
+++ Controller/user.js
@@ -1,51 +1,60 @@
-const User = require('../Models/users')
+const Post = require("../Models/posts");
+const User = require("../Models/users");
 
-module.exports.create = async function(req,res){
-    try{
-        if(req.body.password!=req.body.confirm_password){
-            console.log("password not matched")
-            return res.redirect('back')
-        }
+module.exports.create = async function (req, res) {
+     try {
+          if (req.body.password != req.body.confirm_password) {
+               console.log("password not matched");
+               return res.redirect("back");
+          }
 
-        let user = await User.findOne({email: req.body.email})
+          let user = await User.findOne({ email: req.body.email });
 
-        if(!user){
-            User.create(req.body)
-            return res.redirect('/sign-in')
-        }
+          if (!user) {
+               User.create(req.body);
+               return res.redirect("/sign-in");
+          }
 
-        if(user){
-            return res.redirect('/sign-in')
-        }else{
-            return res.redirect('back')
-        }
+          if (user) {
+               return res.redirect("/sign-in");
+          } else {
+               return res.redirect("back");
+          }
+     } catch (err) {
+          console.log(err);
+     }
+};
 
-    }catch(err){
-        console.log(err)
-    }
-}
+module.exports.createSession = function (req, res) {
+     return res.redirect("/user/dashboard");
+};
 
-module.exports.createSession = function(req,res){
-    return res.redirect('/user/dashboard')
-}
+module.exports.dashboard = async function (req, res) {
+     try {
+          let posts = await Post.find({}).sort({"createdAt":-1}).populate('user');
 
-module.exports.dashboard = function(req,res){
-    return res.render('dashboard',{
-        title: "Dashboard"
-    })
-}
+          if (posts) {
+               return res.render("dashboard", {
+                    title: "Dashboard",
+                    posts: posts,
+               });
+          }
+     } catch (err) {
+          console.log(err);
+     }
+};
 
-module.exports.profile = function(req,res){
-    return res.render("profile",{
-        title: "Profile"
-    })
-}
+module.exports.profile = function (req, res) {
+     return res.render("profile", {
+          title: "Profile",
+     });
+};
 
-module.exports.signOut = function(req,res){
-    req.logout(function (err) {
-        if (err) {
-          return next(err);
-        }
-        res.redirect("/");
-      });
-}
+module.exports.signOut = function (req, res) {
+     req.logout(function (err) {
+          if (err) {
+               return next(err);
+          }
+          res.redirect("/");
+     });
+};
