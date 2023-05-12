bbea9c8f9966810303cb91ab6e5162c2f3ded723
Shivam <shivamguptanitw@gmail.com>
2023-05-12 07:14:17 +0530

    Commit 17: Delete post routes and controller created and working fine.

--- Controller/post.js
+++ Controller/post.js
@@ -1,15 +1,31 @@
-const Post = require("../Models/posts")
+const Comment = require("../Models/comments");
+const Post = require("../Models/posts");
 
-module.exports.create = async function(req,res){
-    try{
-        let post = await Post.create({
-            content: req.body.content,
-            user: req.user._id
-        })
-        return res.redirect('back')
-    }catch(err){
-        console.log(err)
-    }
+module.exports.create = async function (req, res) {
+     try {
+          let post = await Post.create({
+               content: req.body.content,
+               user: req.user._id,
+          });
+          return res.redirect("back");
+     } catch (err) {
+          console.log(err);
+     }
+};
 
-
-}
+module.exports.destroy = async function (req, res) {
+     try {
+          let post = await Post.findById(req.params.id);
+          if (post) {
+               if (post.user == req.user.id) {
+                    post.deleteOne();
+                    await Comment.deleteMany({ post: req.params.id });
+                    return res.redirect("back");
+               }
+          } else {
+               return res.redirect("back");
+          }
+     } catch (err) {
+          console.log(err);
+     } 
+};
--- Routes/post.js
+++ Routes/post.js
@@ -4,5 +4,6 @@ const passport = require('passport');
 const postCtrl = require('../Controller/post')
 
 router.post('/create',passport.checkAuthentication,postCtrl.create)
+router.get('/destroy/:id',passport.checkAuthentication,postCtrl.destroy);
 
 module.exports = router
