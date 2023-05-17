352b14c26971e8808412b59259963b0ae51decc7
Shivam <shivamguptanitw@gmail.com>
2023-05-17 16:42:17 +0530

    Commit 38: Nodemailer template is setup in new  comment mailer and also template is created.

--- Mailers/comments_mailer.js
+++ Mailers/comments_mailer.js
@@ -1,11 +1,13 @@
 const nodemailer = require('../Config/node-mailer')
 
 exports.newComment = (comment) => {
+    let htmlString = nodemailer.renderTemplate({comment: comment},'/new_comments.ejs')
+
     nodemailer.transporter.sendMail({
         from: 'fullstackmerndev@gmail.com',
         to: comment.user.email,
         subject: 'New Commment Posted',
-        html: '<h1>Yup, Your comment is now published<h1/>',
+        html: htmlString,
     },(err,info)=>{
         if(err){
             console.log('Error in sending mail',err);
