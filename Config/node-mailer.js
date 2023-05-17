d49ef36da98ce31b861771c830eb5feaeb8025da
Shivam <shivamguptanitw@gmail.com>
2023-05-17 11:49:24 +0530

    Commit 36: Nodemailer installed and nodemailer conifguation setup done.

---
+++ Config/node-mailer.js
@@ -0,0 +1,32 @@
+const ejs = require("ejs");
+const nodemailer = require("nodemailer");
+const path = require("path");
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+     service: "gmail",
+     host: "smtp.gmail.com",
+     port: 587,
+     secure: false, // true for 465, false for other ports
+     auth: {
+          user: "fullstackmerndev@gmail.com", // generated ethereal user
+          pass: "fullstackmerndev@123", // generated ethereal password
+     },
+});
+
+let renderTemplate = (data, relativePath) => {
+     let mailHTML;
+     ejs.renderFile(path.join(__dirname, "../views/mailers", relativePath), data, function (err, template) {
+          if (err) {
+               console.log("Error in rendering template");
+               return;
+          }
+          mailHTML = template;
+     });
+     return mailHTML;
+};
+
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate,
+}
--- Models/users.js
+++ Models/users.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema(
                type: String,
                required: true,
           },
-          email: {
+          email: { 
                type: String,
                required: true,
                unique: true,
