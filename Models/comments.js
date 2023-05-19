32288c8a713d8a9d41ccafb6666e30a059d6e37c
Shivam <shivamguptanitw@gmail.com>
2023-05-19 10:49:51 +0530

    Commit 40: Likes button added in post and comments views.

--- Models/comments.js
+++ Models/comments.js
@@ -1,29 +1,22 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const commentSchema = new mongoose.Schema(
-     {
-          content: {
-               type: String,
-               required: true,
-          },
-          // comment belongs to a user
-          user: {
-               type: mongoose.Schema.Types.ObjectId,
-               ref: "User",
-          },
-          post: {
-               type: mongoose.Schema.Types.ObjectId,
-               ref: "Post",
-          },
-          likes: {
-               type: mongoose.Schema.Types.ObjectId,
-               ref: "Like",
-          },
-     },
-     {
-          timestamps: true,
-     }
-);
+const commentSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    // comment belongs to a user
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    post:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    }
+},{
+    timestamps: true
+})
 
-const Comment = mongoose.model("Comment", commentSchema);
-module.exports = Comment;
+const Comment = mongoose.model('Comment',commentSchema)
+module.exports = Comment
--- Models/posts.js
+++ Models/posts.js
@@ -16,10 +16,6 @@ const postSchema = new mongoose.Schema(
                     ref: "Comment",
                },
           ],
-          likes:{
-               type: mongoose.Schema.Types.ObjectId,
-               ref: 'Like'
-          },
      },
      {
           timestamps: true,
